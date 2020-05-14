12ecaaff8f6e27e9b377313458e8677238e9a5ca
unknown <viniciustq14@bol.com.br>
2020-05-13 23:43:32 -0300

    um pouco melhor , mas ainda possui erro no insert

--- backend/src/controllers/jogador.js
+++ backend/src/controllers/jogador.js
@@ -2,21 +2,18 @@ const connection = require('../database/connection');
 
 module.exports = {
     async create(req,res) {
-        try {
-            const nm_jogador = req.body
+       
+            const {nm_jogador} = req.body
             if(nm_jogador)
                 console.log(nm_jogador)
             
-        } catch (error) {
-            console.log(`erro no recebimento *${error}*`)
-            return false
-        }
+       
         
         try {
             await connection('jogadores').insert({
                 nm_jogador,
                 domingos_mes_passado: 0,
-                domingos_este_mes: 0
+                domingos_este_mes: 0,
             })
             console.log('certo')
             return res.send('Sucesso')
