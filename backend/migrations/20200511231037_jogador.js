223655f93be147799c3bbaacdd8c2687ec9dddf1
unknown <viniciustq14@bol.com.br>
2020-05-14 22:40:11 -0300

    corrigido

--- backend/migrations/20200511231037_jogador.js
+++ backend/migrations/20200511231037_jogador.js
@@ -1,14 +1,14 @@
 
-exports.up = function(knex) {
-    knex.schema.createTable('jogadores',table=>{
-        table.increments('id_jogador').primary().notNullable();
-        table.string('nm_jogador').notNullable();
-        //um dos fatores e a frequencia do mes anterior e o atual
-        table.integer('domingos_mes_passado').notNullable();
-        table.integer('domingos_este_mes').notNullable();
-    })
+exports.up = function (knex) {
+  return knex.schema.createTable('jogadores', table => {
+    table.increments('id_jogador').primary().notNullable();
+    table.string('nm_jogador').notNullable();
+    //um dos fatores e a frequencia do mes anterior e o atual
+    table.integer('domingos_mes_passado').notNullable();
+    table.integer('domingos_este_mes').notNullable();
+  })
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('jogadores')
+exports.down = function (knex) {
+  return knex.schema.dropTable('jogadores');
 };
