4583f29c506bab4b0a50be2f9de4341ab2f5c871
unknown <viniciustq14@bol.com.br>
2020-05-19 23:19:36 -0300

    dia de jogos adicionado as rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const routes=express.Router()
 const jogador=require('./controllers/jogador')
+const dia_jogo=require('./controllers/diaJogo')
+
 
 routes.get('/',(req,res)=>{
     return res.send('Peladeiros2');
@@ -9,5 +11,9 @@ routes.get('/',(req,res)=>{
 routes.post('/jogador',jogador.create);
 routes.get('/jogadores',jogador.show);
 routes.get('/jogador/:id',jogador.showJogador);
+
+routes.get('/dia',dia_jogo.create)
+routes.get('/dias',dia_jogo.show)
+
 
 module.exports=routes;
