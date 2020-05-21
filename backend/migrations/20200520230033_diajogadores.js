43876215119dc125eeaaffff9b56d387210384bd
unknown <viniciustq14@bol.com.br>
2020-05-20 23:18:28 -0300

    migrate dia e jogadores pronto

--- backend/migrations/20200520230033_diajogadores.js
+++ backend/migrations/20200520230033_diajogadores.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('diaJogadores',table=>{
+      table.integer('id_diaJogo').primary();
+      table.integer('id_jogador').primary();
+      table.integer('qt_partidas').notNullable();
+      table.foreign('id_diaJogo').references('id_jogo').inTable('dia_jogos');
+      table.foreign('id_jogador').references('id_jogador').inTable('jogadores');
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.droptable('diaJogadores');
 };
