3dfb54b288e6bee225d49da491b14ce87d75271c
unknown <viniciustq14@bol.com.br>
2020-05-21 23:27:53 -0300

    adicionada função para integrar o controller diajogador.js

--- backend/src/controllers/diaJogo.js
+++ backend/src/controllers/diaJogo.js
@@ -3,8 +3,11 @@ const connection=require('../database/connection')
 module.exports={
     async create(req,res){
         try {
-            await connection('dia_jogos').insert({})
-            res.send('sucesso')
+            const dt=new Date()
+           
+            await connection('dia_jogos').insert({dt_jogo:dt.getDate(),ic_ativo:true})
+            const [teste]=await connection('dia_jogos').select('id_jogo')
+            res.redirect(`/partidas/${teste}`)
         } catch (error) {
             res.send(`erro ${error}`)
         }
