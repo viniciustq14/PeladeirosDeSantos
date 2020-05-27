ad5e0a9a6fba67bdc63bc16cd6a6729260ee0972
unknown <viniciustq14@bol.com.br>
2020-05-26 23:14:34 -0300

    o redirect ainda nao funcionar ,tentei implementar o delete ainda esta em testes

--- backend/src/controllers/diaJogo.js
+++ backend/src/controllers/diaJogo.js
@@ -7,8 +7,8 @@ module.exports={
            console.log(`${dt.getDate()}/${(dt.getMonth()+1)}/${dt.getFullYear()}`);
             const dia=await connection('dia_jogos').insert({dt_jogo:`${dt.getDate()}/${(dt.getMonth()+1)}/${dt.getFullYear()}`,
             ic_ativo:true})
-            
-            res.redirect(`/partidas/${dia}`)
+            console.log(dia)
+            res.redirect(`/partidas/${dia.id_jogo}`)
         } catch (error) {
             res.send(`erro ${error}`)
         }
@@ -21,5 +21,22 @@ module.exports={
         } catch (error) {
             res.send(`erro ${error}`)
         }
-    }
+    },
+
+    async deletar(req,res){
+        try {
+            const [id]=req.params
+            console.log(id);
+        } catch (error) {
+            console.log(error)
+        }
+        
+        try {
+            
+            await connection('dia_jogos').where('id_jogo', id).delete();
+            res.send('deletado');
+        } catch (error) {
+            res.send(`${error}`);
+        }
+    },
 }
