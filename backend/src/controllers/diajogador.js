273a757ee52466d53cfa81054cf18388d626056e
unknown <viniciustq14@bol.com.br>
2020-05-28 23:15:16 -0300

    o insert ainda possui falhas no contexto devido a estrutura da tabela

--- backend/src/controllers/diajogador.js
+++ backend/src/controllers/diajogador.js
@@ -2,18 +2,21 @@ const connection=require('../database/connection')
 
 module.exports={
     async convocar(req,res){
-        const {id_jogo}=req.params;
+        const id_jogo=req.params;
         const {id_jogador}=req.body;
+
+        console.log(id_jogador)
+        console.log(id_jogo)
 
         try {
             await connection('diaJogadores').insert({
-                id_diaJogo:id_jogo,
+                id_diaJogo:id_jogo.id,
                 id_jogador,
                 qt_partidas:0
             })
 
-            const jogadores=await connection('diaJogadores').where('id_diaJogo',id_jogo).select('*')
-            res.send('convocado',jogadores)
+           // const jogadores=await connection('diaJogadores').where('id_diaJogo',id_jogo).select('*')
+            res.send('convocado')
         } catch (error) {
             res.send(` erroo : ${error}`);
         }
@@ -21,7 +24,12 @@ module.exports={
     },
 
     async show(req,res){
-        const {id_jogo}=req.params
-        res.send(`dia de jogo ${id_jogo}`)
+        const id_jogo=req.params;
+        console.log(id_jogo.id);
+
+        const tabelas=await connection('diaJogadores').select('*');
+        console.log(tabelas)
+
+        res.send(`dia de jogo ${id_jogo.id}`)
     }
 }
