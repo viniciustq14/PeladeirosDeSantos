a5a1986e971f3deebaabe4a8f0e975594a6132c3
unknown <viniciustq14@bol.com.br>
2020-06-05 23:30:49 -0300

    nova estrutura

--- backend/migrations/20200520230033_diajogadores.js
+++ backend/migrations/20200520230033_diajogadores.js
@@ -1,15 +1,16 @@
 
-exports.up = function(knex) {
-  return knex.schema.createTable('diaJogadores',table=>{
-      table.integer('id_diaJogo').primary();
-      table.integer('id_jogador').primary();
-      table.integer('qt_partidas').notNullable();
-      table.timestamp('dt_escalado').defaultTo(knex.fn.now());
-      table.foreign('id_diaJogo').references('id_jogo').inTable('dia_jogos');
-      table.foreign('id_jogador').references('id_jogador').inTable('jogadores');
+exports.up = function (knex) {
+  return knex.schema.createTable('diaJogadores', table => {
+    table.increments('id').notNullable();
+    table.integer('id_diaJogo').notNullable();
+    table.integer('id_jogador').notNullable();
+    table.integer('qt_partidas').notNullable();
+    table.timestamp('dt_escalado').defaultTo(knex.fn.now());
+    table.foreign('id_diaJogo').references('id_diaJogo').inTable('dia_jogos');
+    table.foreign('id_jogador').references('id_jogador').inTable('jogadores');
   })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.droptable('diaJogadores');
 };
