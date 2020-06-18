9e48a547c3c33f0e47029f0a294e5829cdf03ca2
unknown <viniciustq14@bol.com.br>
2020-06-17 23:10:38 -0300

    inserindo a  pagina inicial

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Principal from './principal/principal' 
 import './App.css';
 
 function App() {
   return (
-    <div>
-      teste
-      </div>
+   
+      <Principal/>
+   
   );
 }
 
