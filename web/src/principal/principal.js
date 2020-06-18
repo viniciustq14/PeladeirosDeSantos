0779088c8c17ef2acfe7c356b848769354c89283
unknown <viniciustq14@bol.com.br>
2020-06-17 23:09:56 -0300

    pagina inicial

---
+++ web/src/principal/principal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css';
+
+function Principal() {
+  return (
+    <header>
+        <h1>Peladeiros De Santos</h1>
+    </header>
+  );
+}
+
+export default Principal;
