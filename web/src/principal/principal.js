516b42a0ebc58d384a06da533b725ed1ae1af80f
unknown <viniciustq14@bol.com.br>
2020-06-22 23:09:54 -0300

    adicionado marcações

--- web/src/principal/principal.js
+++ web/src/principal/principal.js
@@ -4,9 +4,23 @@ import './style.css';
 
 function Principal() {
   return (
+    <div className='cabecalho'>
     <header>
         <h1>Peladeiros De Santos</h1>
     </header>
+    <div className='menu'>
+      <div>
+        <h2>jogadores</h2>
+        
+      </div>
+      <div>
+        <h2>
+        dias de partidas
+
+        </h2>
+      </div>
+    </div>
+      </div>
   );
 }
 
