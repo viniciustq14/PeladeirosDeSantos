8f40475a3a613efb0c5e16216bc9aad6c0e93581
unknown <viniciustq14@bol.com.br>
2020-06-30 22:55:23 -0300

    tranferencia na parte do cabeçalho

--- web/src/principal/principal.js
+++ web/src/principal/principal.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './style.css';
-import Cabecalho from '../cabecalho/Cabecalho'
+
 
 function Principal() {
   return (
     <>
-      <Cabecalho />
+     
       <div className="botoes">
         <button>Cadastrar Jogadores</button>
         <button>Iniciar Dia De Jogo </button>
