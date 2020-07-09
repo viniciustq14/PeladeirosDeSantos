a929a71af8be59e8ff77b679ffccd6765c676ccc
unknown <viniciustq14@bol.com.br>
2020-07-08 22:10:53 -0300

    link para pagina de jogadores foi implementada

--- web/src/cabecalho/Cabecalho.js
+++ web/src/cabecalho/Cabecalho.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './style.css';
 
-function Cabecalho( ){
-    return(
+function Cabecalho() {
+    return (
         <div className='cabecalho'>
-             <header>
+            <header>
                 <h1>Peladeiros De Santos</h1>
             </header>
             <div className='menu'>
-                <div id='jogadores'>
-                    <h2>jogadores</h2>
-                </div>
+                <Link to='jogador'>
+                    <div id='jogadores'>
+                        <h2>jogadores</h2>
+                    </div>
+                </Link>
                 <div id='jogo'>
                     <h2>
                         dias de partidas
