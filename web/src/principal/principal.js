47cb357fe711099267423f86270d5f2b1f8e667f
unknown <viniciustq14@bol.com.br>
2020-07-08 22:09:55 -0300

    link removido, arquivo errado

--- web/src/principal/principal.js
+++ web/src/principal/principal.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+
 import './style.css';
 
 
 function Principal() {
   return (
     <>
-     
+
       <div className="botoes">
-      <Link to='jogador'>
+
         <button>Cadastrar Jogadores</button>
-      </Link>
         <button>Iniciar Dia De Jogo </button>
 
       </div>
